eddbfc49d1f0921b3da8b2a7e93922c7d64f4ad8
David Sosa <danotron74@gmail.com>
2020-12-06 01:18:14 -0500

    sección de noticias con APIs

---
+++ src/components/SeccionApis.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+    name: 'SeccionApis',
+    data(){
+        return{
+            movies: null
+        }
+    },
+    mounted(){
+        fetch('http://www.omdbapi.com/?t=guardianes&apikey=ec274c35')
+        .then(response => {
+            (this.info = response.data.Search) 
+            console.log(this.info.data)
+            })
+    }
+}
+</script>
